971cbd0fae22d99d46d36db66d3702871b8f272c
Carlos Tacón <carlosjtacon@gmail.com>
2018-05-30 18:40:52 +0200

    changed asset details config

--- Plugins/LeCorbusier/Source/LeCorbusier/Private/Types/ULCAsset.h
+++ Plugins/LeCorbusier/Source/LeCorbusier/Private/Types/ULCAsset.h
@@ -28,8 +28,8 @@ public:
 		MaxInstances(0),
 		Probability(0.7f),
 		Radius(0),
-		Scale(FFloatInterval(1,1)),
-		Rotation(FFloatInterval(0, 0)),
+		ScaleXYZ(FFloatInterval(1,1)),
+		RotationZ(FFloatInterval(0, 0)),
 		TranslationX(FFloatInterval(0, 0)),
 		TranslationY(FFloatInterval(0, 0)),
 		TranslationZ(FFloatInterval(0, 0))
@@ -47,42 +47,42 @@ public:
 	// GENERAL SETTINGS
 
 	/** Enable this asset */
-	UPROPERTY(EditAnywhere, Category = General, meta = (DisplayName = "Enable"))
+	UPROPERTY(EditAnywhere, Category = Instance, meta = (DisplayName = "Enable"))
 	bool bEnable;
 
 	/** Maximum number of instances generated. Zero is unlimited! */
-	UPROPERTY(EditAnywhere, Category = General, meta = (DisplayName = "Max. Instances", ClampMin = 0))
+	UPROPERTY(EditAnywhere, Category = Instance, meta = (DisplayName = "Max. Instances", ClampMin = 0))
 	uint32 MaxInstances;
 	
 	/** Probability of appearance */
-	UPROPERTY(EditAnywhere, Category = General, meta = (DisplayName = "Probability", ClampMin = 0, ClampMax = 1))
+	UPROPERTY(EditAnywhere, Category = Instance, meta = (DisplayName = "Probability", ClampMin = 0, ClampMax = 1))
 	float Probability;
 
-
-	// PLACEMENT SETTINGS
-	
 	/** Minimum distance between instances */
-	UPROPERTY(EditAnywhere, Category = Placement, meta = (DisplayName = "Radius", ClampMin = 0))
+	UPROPERTY(EditAnywhere, Category = Instance , meta = (DisplayName = "Radius", ClampMin = 0))
 	float Radius;
 
-	/** Scale adjustment interval */
-	UPROPERTY(EditAnywhere, Category = Placement, meta = (DisplayName = "Scale"))
-	FFloatInterval Scale;
 
-	/** Rotation adjustment interval */
-	UPROPERTY(EditAnywhere, Category = Placement, meta = (DisplayName = "Rotation"))
-	FFloatInterval Rotation;
+	// TRANSFORMATION SETTINGS
+
+	/** Scale adjustment interval (XYZ) */
+	UPROPERTY(EditAnywhere, Category = Transformation, meta = (DisplayName = "Scale"))
+	FFloatInterval ScaleXYZ;
+
+	/** Rotation adjustment interval (Z) */
+	UPROPERTY(EditAnywhere, Category = Transformation, meta = (DisplayName = "Rotation"))
+	FFloatInterval RotationZ;
 	
 	/** Translation offset interval (X) */
-	UPROPERTY(EditAnywhere, Category = Placement, meta = (DisplayName = "Translation X"))
+	UPROPERTY(EditAnywhere, Category = Transformation, meta = (DisplayName = "Translation X"))
 	FFloatInterval TranslationX;
 
 	/** Translation offset interval (Y) */
-	UPROPERTY(EditAnywhere, Category = Placement, meta = (DisplayName = "Translation Y"))
+	UPROPERTY(EditAnywhere, Category = Transformation, meta = (DisplayName = "Translation Y"))
 	FFloatInterval TranslationY;
 
 	/** Translation offset interval (Z) */
-	UPROPERTY(EditAnywhere, Category = Placement, meta = (DisplayName = "Translation X"))
+	UPROPERTY(EditAnywhere, Category = Transformation, meta = (DisplayName = "Translation X"))
 	FFloatInterval TranslationZ;
 	
 };
