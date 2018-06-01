51e7fce94c3e8c41545a1610d45caf6bac926bf7
Carlos Tacón <carlosjtacon@gmail.com>
2018-06-01 18:59:35 +0200

    get floor surface and object real z-position

--- Plugins/LeCorbusier/Source/LeCorbusier/Private/Generator/LCGenerator.cpp
+++ Plugins/LeCorbusier/Source/LeCorbusier/Private/Generator/LCGenerator.cpp
@@ -2,42 +2,34 @@
 
 #define LOCTEXT_NAMESPACE "LCGeneratorNamespace"
 
+
+/** AVAILABLE ALGORITHMS */
 void LCGenerator::CreateEnvironmentRandom(TArray<ULCAsset*> Items)
 {
 	UE_LOG(LogTemp, Warning, TEXT("BUILDING ENVIRONMENT RANDOM"));
 	// Let editor know that we're about to do something that we want to undo/redo
 	GEditor->BeginTransaction(LOCTEXT("CreateEnvironmentRandomTransaction", "Generate Environment"));
-
-	// Get selected actor as surface 
-	TArray<UObject*> SelectedActors;
-	GEditor->GetSelectedActors()->GetSelectedObjects(AActor::StaticClass(), SelectedActors);
-	AActor* SelectedActor = Cast<AActor>(SelectedActors[0]);
-	UE_LOG(LogTemp, Warning, TEXT("Building environment using %s as surface"), *SelectedActor->GetName());
 	
-	// Bounds Origin = center of box
-	// Bounds Extent = distance of bounds from origin
-	FVector Origin, Extent;
-	SelectedActor->GetActorBounds(false, Origin, Extent);
-	//SelectedActor->GetTransform().DebugPrint();
-	UE_LOG(LogTemp, Warning, TEXT("SelectedActor's Location is %s"), *SelectedActor->GetActorLocation().ToString());
-	UE_LOG(LogTemp, Warning, TEXT("SelectedActor's Rotation is %s"), *SelectedActor->GetActorRotation().ToString());
-	UE_LOG(LogTemp, Warning, TEXT("SelectedActor's Scale is %s"), *SelectedActor->GetActorScale().ToString());
-	UE_LOG(LogTemp, Warning, TEXT("SelectedActor's Bounds Origin is %s"), *Extent.ToString());
-	UE_LOG(LogTemp, Warning, TEXT("SelectedActor's Bounds Extent is %s"), *Origin.ToString());
+	// Get surface coordinates
+	FBox FloorSurface = GetFloorSurface();
+	UE_LOG(LogTemp, Warning, TEXT("FloorSurface is %s"), *FloorSurface.ToString());
 
 	// Get item from list
 	UObject* AssetObject = Items[0]->Asset;
-	UStaticMesh* myStaticMesh = Cast<UStaticMesh>(AssetObject);
-	UE_LOG(LogTemp, Warning, TEXT("myStaticMesh's BoundingBox is %s"), *myStaticMesh->GetBoundingBox().GetSize().ToString());
-
+	UStaticMesh* MyStaticMesh = Cast<UStaticMesh>(AssetObject);
+	// PrintDebugUStaticMesh(MyStaticMesh);
+	
+	// Calculate needed variables
+	float RealObjectZ = FloorSurface.Min.Z - MyStaticMesh->GetBoundingBox().Min.Z;
+	
 	// Generate actor location, rotation and scale
-	FVector objectPosition(0, 0, 0);
+	FVector objectPosition(0, 0, RealObjectZ);
 	FRotator objectRotation(0, 0, 0);
 	FVector objectScale(1, 1, 1);
 	FTransform objectTrasform(objectRotation, objectPosition, objectScale);
 	
 	// Place actor into the level
-	PlacingStaticMesh(myStaticMesh, objectTrasform);
+	PlacingStaticMesh(MyStaticMesh, objectTrasform);
 
 	// We're done generating the environment so we close the transaction
 	GEditor->EndTransaction();
@@ -53,6 +45,37 @@ void LCGenerator::CreateEnvironmentCity(TArray<ULCAsset*> Items, uint32 NumNatur
 {
 	UE_LOG(LogTemp, Warning, TEXT("BUILDING ENVIRONMENT CITY (NumNatureAreas: %d, NaturePercentage: %f)"), NumNatureAreas, NaturePercentage);
 
+}
+
+
+/** UTILS METHODS */
+FBox LCGenerator::GetFloorSurface()
+{
+	// Get selected actor as surface 
+	TArray<UObject*> SelectedActors;
+	GEditor->GetSelectedActors()->GetSelectedObjects(AActor::StaticClass(), SelectedActors);
+	AActor* SelectedActor = Cast<AActor>(SelectedActors[0]);
+	
+	// PrintDebugAActor(SelectedActor);
+
+	FVector MinPoint, MaxPoint;
+	if (SelectedActor->GetClass()->IsChildOf(AStaticMeshActor::StaticClass()))
+	{
+		// UE_LOG(LogTemp, Error, TEXT("IT IS A STATIC MESH"));
+		MinPoint = SelectedActor->GetComponentsBoundingBox().Min;
+		MaxPoint = SelectedActor->GetComponentsBoundingBox().Max;
+		MinPoint.Z = SelectedActor->GetComponentsBoundingBox().Max.Z;
+	}
+	else if (SelectedActor->GetClass()->IsChildOf(ATriggerBox::StaticClass()))
+	{
+		// UE_LOG(LogTemp, Error, TEXT("IT IS A TRIGGER BOX"));
+		MinPoint = SelectedActor->GetComponentsBoundingBox().Min;
+		MaxPoint = SelectedActor->GetComponentsBoundingBox().Max;
+		MaxPoint.Z = SelectedActor->GetComponentsBoundingBox().Min.Z;
+	}
+
+	FBox FloorSurface(MinPoint, MaxPoint);
+	return FloorSurface;
 }
 
 void LCGenerator::PlacingStaticMesh(UStaticMesh* myStaticMesh, FTransform objectTrasform)
@@ -77,6 +100,40 @@ void LCGenerator::PlacingStaticMesh(UStaticMesh* myStaticMesh, FTransform object
 	currentWorld->UpdateWorldComponents(true, false);
 	smActor->RerunConstructionScripts();
 	GLevelEditorModeTools().MapChangeNotify();
+}
+
+
+/** DEBUG METHODS */
+void LCGenerator::PrintDebugAActor(AActor* Actor)
+{
+	FString ActorName = *Actor->GetName();
+	
+	FVector Origin, Extent;
+	Actor->GetActorBounds(false, Origin, Extent);
+
+	UE_LOG(LogTemp, Error, TEXT("AActor: %s"), *ActorName);
+	UE_LOG(LogTemp, Warning, TEXT("%s's Location is %s"), *ActorName, *Actor->GetActorLocation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's Rotation is %s"), *ActorName, *Actor->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's Scale is %s"), *ActorName, *Actor->GetActorScale().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's Bounds/Origin is %s"), *ActorName, *Extent.ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's Bounds/Extent is %s"), *ActorName, *Origin.ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's BoundingBox is %s"), *ActorName, *Actor->GetComponentsBoundingBox().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's BoundingBox Extent is %s"), *ActorName, *Actor->GetComponentsBoundingBox().GetExtent().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's BoundingBox Size is %s"), *ActorName, *Actor->GetComponentsBoundingBox().GetSize().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's BoundingBox Center is %s"), *ActorName, *Actor->GetComponentsBoundingBox().GetCenter().ToString());
+
+	//Actor->GetTransform().DebugPrint();
+}
+
+void LCGenerator::PrintDebugUStaticMesh(UStaticMesh* StaticMesh)
+{
+	FString StaticMeshName = *StaticMesh->GetName();
+
+	UE_LOG(LogTemp, Error, TEXT("UStaticMesh: %s"), *StaticMeshName);
+	UE_LOG(LogTemp, Warning, TEXT("%s's BoundingBox is %s"), *StaticMeshName, *StaticMesh->GetBoundingBox().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's BoundingBox Extent is %s"), *StaticMeshName, *StaticMesh->GetBoundingBox().GetExtent().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's BoundingBox Size is %s"), *StaticMeshName, *StaticMesh->GetBoundingBox().GetSize().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("%s's BoundingBox Center is %s"), *StaticMeshName, *StaticMesh->GetBoundingBox().GetCenter().ToString());
 }
 
 #undef LOCTEXT_NAMESPACE
--- Plugins/LeCorbusier/Source/LeCorbusier/Private/Generator/LCGenerator.h
+++ Plugins/LeCorbusier/Source/LeCorbusier/Private/Generator/LCGenerator.h
@@ -6,6 +6,7 @@
 #include "Engine/Selection.h"
 #include "Editor/EditorEngine.h"
 #include "Runtime/Engine/Classes/Engine/StaticMeshActor.h"
+#include "Runtime/Engine/Classes/Engine/TriggerBox.h"
 
 #include "../Types/ULCAsset.h"
 
@@ -13,11 +14,21 @@ class LCGenerator
 {
 public:
 
+	/** AVAILABLE ALGORITHMS */
 	void CreateEnvironmentRandom(TArray<ULCAsset*> Items);
 	void CreateEnvironmentNature(TArray<ULCAsset*> Items, bool bMixDifferentTrees);
 	void CreateEnvironmentCity(TArray<ULCAsset*> Items, uint32 NumNatureAreas, float NaturePercentage);
 
 private:
 
+	FMath MathAdvanced;
+	FGenericPlatformMath MathBasics;
+
+	/** UTILS METHODS */
+	FBox GetFloorSurface();
 	void PlacingStaticMesh(UStaticMesh* myStaticMesh, FTransform objectTrasform);
+	
+	/** DEBUG METHODS */
+	void PrintDebugAActor(AActor* Actor);
+	void PrintDebugUStaticMesh(UStaticMesh* StaticMesh);
 };
